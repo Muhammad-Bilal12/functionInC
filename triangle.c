3271d85c3d09c8454cd066a3098bc755f6c94c76
bilal <muhammadbilalmnaeem@gmail.com>
2022-05-31 09:35:07 +0500

    area of square and triangle

---
+++ triangle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+
+float triangle();
+void main()
+{
+    printf("The Area of Triangle is %.2f sq unit", triangle());
+}
+
+float triangle()
+{
+    float base, height;
+    printf("Find Area of Triangle \n");
+    printf("Height of Triangle : ");
+    scanf("%f", &height);
+    printf("Base of Triangle : ");
+    scanf("%f", &base);
+
+    return (height * base) / 2;
+}
